c81d63309d54b4fc9205b4ac6d889d50605d46c0
Kelvin Wang <sherkrainwang@gmail.com>
2019-11-13 11:02:17 -0500

    chore(src): refactor

---
+++ src/components/Client.ts
@@ -0,0 +1,116 @@
+import { workspace, ExtensionContext, window } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  DidOpenTextDocumentNotification,
+  DidSaveTextDocumentNotification
+} from "vscode-languageclient";
+
+export class Client {
+  private languageClient: LanguageClient;
+
+  // constructor
+  constructor(lspPath: string, logFilePath: string) {
+    let cmd = lspPath;
+    let debugArgs = ["-l", logFilePath];
+
+    let serverOptions: ServerOptions = {
+      run: {
+        command: cmd
+      },
+      debug: {
+        command: cmd,
+        args: debugArgs
+      }
+    };
+
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+      // Register the server for flux documents
+      documentSelector: [{ scheme: "file", language: "flux" }],
+      synchronize: {
+        // Notify the server about file changes to '.clientrc files contained in the workspace
+        fileEvents: workspace.createFileSystemWatcher("**/.clientrc")
+      }
+    };
+
+    // Create the language client and start the client.
+    this.languageClient = new LanguageClient(
+      "flux lsp server",
+      "Flux Language Server",
+      serverOptions,
+      clientOptions
+    );
+  }
+
+  start(context: ExtensionContext): void {
+    this.actOnOpen(context);
+    this.actOnSave(context);
+    this.languageClient.start();
+
+    console.log("flux lsp client started");
+  }
+
+  stop(): Thenable<void> | undefined {
+    console.log("deactived flux plugin");
+    if (!this.languageClient) {
+      return undefined;
+    }
+    return this.languageClient.stop();
+  }
+
+  private actOnOpen(context: ExtensionContext): void {
+    context.subscriptions.push(
+      workspace.onDidOpenTextDocument(document => {
+        if (
+          document.languageId !== "flux" ||
+          !document.fileName.endsWith(".flux")
+        ) {
+          return;
+        }
+        this.languageClient.sendNotification(
+          DidOpenTextDocumentNotification.type,
+          {
+            textDocument: {
+              uri: document.uri.toString(),
+              languageId: document.languageId,
+              text: document.getText(),
+              version: document.version
+            }
+          }
+        );
+      })
+    );
+  }
+
+  private actOnSave(context: ExtensionContext): void {
+    context.subscriptions.push(
+      workspace.onDidSaveTextDocument(document => {
+        if (!window.activeTextEditor) {
+          return;
+        }
+        const activeDocument = window.activeTextEditor.document;
+        if (document !== activeDocument) {
+          return;
+        }
+        if (
+          document.languageId !== "flux" ||
+          !document.fileName.endsWith(".flux")
+        ) {
+          return;
+        }
+        this.languageClient.sendNotification(
+          DidSaveTextDocumentNotification.type,
+          {
+            textDocument: {
+              uri: document.uri.toString(),
+              version: document.version
+            }
+          }
+        );
+      })
+    );
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,95 +1,28 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import * as path from "path";
-import { workspace, ExtensionContext, window } from "vscode";
+
+import { Client } from "./components/Client";
+
+let client: Client;
 
 import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions
-} from "vscode-languageclient";
-
-let client: LanguageClient;
-
-import { DidSaveTextDocumentNotification } from "vscode-languageserver-protocol";
+  DidSaveTextDocumentNotification,
+  DidOpenTextDocumentNotification
+} from "vscode-languageserver-protocol";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // The server is implemented in rust
   let cmd = "/usr/local/sbin/flux-lsp";
-  let debugArgs = ["-l", "/tmp/lsp.log"];
+  let logFilePath = "/tmp/lsp.log";
 
-  let serverOptions: ServerOptions = {
-    run: {
-      command: cmd
-    },
-    debug: {
-      command: cmd,
-      args: debugArgs
-    }
-  };
-
-  // Options to control the language client
-  let clientOptions: LanguageClientOptions = {
-    // Register the server for flux documents
-    documentSelector: [{ scheme: "file", language: "flux" }],
-    synchronize: {
-      // Notify the server about file changes to '.clientrc files contained in the workspace
-      fileEvents: workspace.createFileSystemWatcher("**/.clientrc")
-    }
-  };
-
-  // Create the language client and start the client.
-  client = new LanguageClient(
-    "flux lsp server",
-    "Flux Language Server",
-    serverOptions,
-    clientOptions
-  );
-
-  // Start the client. This will also launch the server
-  client.start();
-
-  addExecutingActionOnSave(context, client);
-  console.log("flux lsp client started");
+  client = new Client(cmd, logFilePath);
+  client.start(context);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate(): Thenable<void> | undefined {
-  console.log("deactived plugin");
-  if (!client) {
-    return undefined;
-  }
   return client.stop();
-}
-
-function addExecutingActionOnSave(
-  context: ExtensionContext,
-  client: LanguageClient
-): void {
-  context.subscriptions.push(
-    workspace.onDidSaveTextDocument(document => {
-      if (!window.activeTextEditor) {
-        return;
-      }
-      const activeDocument = window.activeTextEditor.document;
-      if (document !== activeDocument) {
-        return;
-      }
-      if (
-        document.languageId !== "flux" ||
-        !document.fileName.endsWith(".flux")
-      ) {
-        return;
-      }
-      client.sendNotification(DidSaveTextDocumentNotification.type, {
-        textDocument: {
-          uri: document.uri.toString(),
-          version: document.version
-        }
-      });
-    })
-  );
 }
