166a473a5d874d785033baa7fab9cebbdd2a922f
Sean Brickley <sean@wabr.io>
2021-06-01 10:49:53 -0400

    chore: Consistent formatting

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 // import {Stream} from 'stream';
 
 declare module '@influxdata/flux-lsp-cli' {
-	export default class CLI {
-		constructor(options : { 'disable-folding' : boolean })
-		createStream : () => any
-		on : (event : 'log', cb : (msg : string) => void) => void
-		registerBucketsCallback : (fn : () => Promise<string[]>) => void
-	}
+    export default class CLI {
+        constructor(options : { 'disable-folding' : boolean })
+        createStream : () => any
+        on : (event : 'log', cb : (msg : string) => void) => void
+        registerBucketsCallback : (fn : () => Promise<string[]>) => void
+    }
 }
