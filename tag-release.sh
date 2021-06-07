bd4bf7b6654b357762327473bd36420b92d72995
Sean Brickley <sean@wabr.io>
2021-06-07 15:17:03 -0400

    chore: remove unnecessary checks

--- tag-release.sh
+++ tag-release.sh
@@ -7,32 +7,12 @@ if [[ ! $hub_installed ]]; then
 	exit 1
 fi
 
-current_branch=$(git branch --show-current)
-if [[ $current_branch != "master" ]]; then
-	echo "This script should only be run from the master branch. Aborting."
-	exit 1
-fi
-
-git_changes=$(git status -s | wc -l)
-if [[ $git_changes != 0 ]]; then
-	echo "The master branch has been modified."
-	echo "Please revert the changes or move them to another branch before running this script."
-	exit 1
-fi
-
-git fetch
-ahead=$(git status -sb | grep ahead -c)
-if [[ $ahead != 0 ]]; then
-	echo "Your local master branch is ahead of the remote master branch. Aborting."
-	exit 1
-fi
-
 new_version=v$(grep -Eom 1 "([0-9]{1,}\.)+[0-9]{1,}" package.json)
 
 git tag -a -s $new_version -m "Release $new_verion"
 git push origin master $new_version
 
-lsp_version=v$(grep -E -m 1 '"@influxdata/flux-lsp-node":' package.json | grep -Eo "([0-9]{1,}\.)+[0-9]{1,}")
+lsp_version=v$(grep -m 1 '"@influxdata/flux-lsp-node":' package.json | grep -Eo "([0-9]{1,}\.)+[0-9]{1,}")
 
 hub release create $new_version -m "Release $new_version
 
