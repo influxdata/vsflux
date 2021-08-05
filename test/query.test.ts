4ed8d5d284ee6752a7d548c41af018f0557ae88f
Paul Hummer <rockstar@users.noreply.github.com>
2021-08-05 11:23:19 -0600

    fix: handle error tables (#260)
    This patch adds support for displaying error tables in the table view.
    This ollows malformed queries that error to be displayed similar to how
    they would be displayed in the web ui.
    
    Closes #224

--- test/query.test.ts
+++ test/query.test.ts
@@ -13,10 +13,23 @@ describe('transform query results', () => {
     const results = queryResponseToTableResult(contents)
     const [first, second] = results
 
-    expect(first.head[0]).toEqual('table')
+    expect(first.head).toEqual(['table', '_start', '_stop', '_time', '_value', '_field', '_measurement', 'host', 'name'])
     expect(first.rows.length).toEqual(9)
-    expect(second.head[0]).toEqual('table')
+    expect(second.head).toEqual(['table', '_start', '_stop', '_time', '_value', '_field', '_measurement', 'host'])
     expect(second.rows.length).toEqual(1)
     expect(results.length).toEqual(2)
+  })
+
+  it('handles error tables properly', () => {
+    const contents = fs.readFileSync(
+      `${__dirname}/fixtures/error.csv`, { encoding: 'utf8' }
+    )
+    const results = queryResponseToTableResult(contents)
+    const [first, second] = results
+
+    expect(first.head).toEqual(['table', 'url'])
+    expect(first.rows).toEqual([['0', 'not_gonna_work.bar']])
+    expect(second.head).toEqual(['error', 'reference'])
+    expect(second.rows).toEqual([['"runtime error @18:4-18:60: map: failed to evaluate map function: Post ""not_gonna_work.bar"": unsupported protocol scheme """""', '']])
   })
 })
